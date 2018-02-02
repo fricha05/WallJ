4e82b03af06ecdea486828b4c55d27a83f25eae4
Florian <florian@fixe-florian.home>
2018-02-02 15:03:39 +0100

    essais pathfinding

--- src/fr/umlv/data/main/Main.java
+++ src/fr/umlv/data/main/Main.java
@@ -1,16 +1,141 @@
 package fr.umlv.data.main;
 
-import java.awt.Color;
-//import java.awt.geom.Ellipse2D;
-import java.awt.geom.Rectangle2D;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 import fr.umlv.data.canvas.*;
-import fr.umlv.data.graphics.Display;
-import fr.umlv.zen5.*;
 import pathfinding.Node;
 import pathfinding.Nodelist;
 
 public class Main {
+	
+	public static Node[][] initNodeMap(Level lvl, Node start, Node end) {
+		Node[][] nodeTab = new Node[lvl.nbLine][lvl.nbCol];
+		for (int i = 0; i < lvl.nbLine ; i++) {
+			for (int j = 0; j < lvl.nbCol; j++) {
+				final String s = lvl.getLvl()[i][j].toString();
+				if(s == " "){
+					nodeTab[i][j] = new Node(lvl.getLvl()[i][j], start.getCurrent(), end.getCurrent(), null);
+				}
+				else if(s == " " && lvl.getLvl()[i][j].equals(end.getCurrent())) {
+					nodeTab[i][j] = end;
+				}
+				else if(s == "R") {
+					nodeTab[i][j] = start;
+				}
+				else {
+					nodeTab[i][j] = null;
+				}
+			}
+		}
+		return nodeTab;
+	}
+	
+	public static Node getLowest(ArrayList<Node> list) {
+		Node lowest = list.get(0);
+		for(int i = 0; i < list.size() - 1; i++) {
+			if(list.get(i).getQuality() > list.get(i+1).getQuality()) {
+				lowest = list.get(i+1);
+			}
+		}
+//		System.out.println(lowest);
+		return lowest;
+	}
+	
+	public static List<Node> calcPath(Node start, Node goal){
+		LinkedList<Node> path = new LinkedList<Node>();
+		
+		Node curr = goal;
+		boolean done = false;
+		while(!done) {
+			path.addFirst(curr);
+			curr = curr.getParent();
+			if(curr.equals(start)) {
+				done = true;
+			}
+		}
+		return path;
+		
+	}
+	
+	public static ArrayList<Node> getNeighbors(Node[][] nodeMap, Node current){
+		ArrayList<Node> neighbors = new ArrayList<Node>();
+		if(nodeMap[current.getCurrent().getPosX() +1][current.getCurrent().getPosY()]!=null) {
+			neighbors.add(nodeMap[current.getCurrent().getPosX() +1][current.getCurrent().getPosY()]);
+		}
+		if(nodeMap[current.getCurrent().getPosX()][current.getCurrent().getPosY() +1]!=null) {
+			neighbors.add(nodeMap[current.getCurrent().getPosX()][current.getCurrent().getPosY() +1]);
+		}
+		if(nodeMap[current.getCurrent().getPosX() -1][current.getCurrent().getPosY()]!=null) {
+			neighbors.add(nodeMap[current.getCurrent().getPosX() -1][current.getCurrent().getPosY()]);
+		}
+		if(nodeMap[current.getCurrent().getPosX()][current.getCurrent().getPosY() -1]!=null) {
+			neighbors.add(nodeMap[current.getCurrent().getPosX()][current.getCurrent().getPosY() -1]);
+		}
+		
+		return neighbors;
+	}
+	
+	public static void printNodeMap(Node[][] nodeMap) {
+		for (int i = 0; i < 13; i++) {
+			for (int j = 0; j < 58; j++) {
+				if(nodeMap[i][j] == null) {
+					System.out.print("N");
+				}
+				else if(nodeMap[i][j].getCurrent().toString() == " ") {
+					System.out.print(" ");
+				}
+				else if(nodeMap[i][j].getCurrent().toString() == "R") {
+					System.out.print("R");
+				}
+			}
+			System.out.println();
+		}
+	}
+	
+	public final static List<Node> findPath(Node start, Node goal, Level lvl) {
+		Node[][] nodeMap = initNodeMap(lvl, start, goal);
+		printNodeMap(nodeMap);
+		ArrayList<Node> openList = new ArrayList<Node>();
+		ArrayList<Node> closedList = new ArrayList<Node>();
+		openList.add(start);
+		
+		boolean done = false;
+		Node current;
+		while(!done) {
+			current = getLowest(openList);
+			closedList.add(current);
+			
+			if((current.equals(goal))) {
+				return calcPath(start, goal);
+			}
+			
+			ArrayList<Node> neighbors = getNeighbors(nodeMap, current);
+			for(int i = 0; i < neighbors.size(); i++) {
+				Node currNeighbor = neighbors.get(i);
+				if(!openList.contains(currNeighbor)) {
+					currNeighbor.setParent(current);
+					openList.add(currNeighbor);
+				}
+				else {
+					if(currNeighbor.getCostStart() > current.getCostStart()) {
+						currNeighbor.setParent(current);
+					}
+				}
+			}
+			if(openList.isEmpty()) {
+				return new LinkedList<Node>();
+			}
+		}
+		System.out.println("Point non accessible");
+		return null; //non accessible
+		
+		
+	}
+	
 	public static void main(String[] args) throws IOException {
 		Level lvl1 = new Level(13, 58);
 		String lvlpath = "files/lvl1.txt";
@@ -20,7 +145,7 @@ public class Main {
 		/*Application.run(Color.BLACK, context ->{
 			ScreenInfo screenInfo = context.getScreenInfo();
 		    float width = screenInfo.getWidth();
-		    float height = screenInfo.getHeight();
+		    float height = screenInfo.getHeight(System.out.println(openList););
 		    System.out.println("size of the screen (" + width + " x " + height + ")");
 		    context.renderFrame(graphics -> {
 		        graphics.setColor(Color.YELLOW);
@@ -39,10 +164,10 @@ public class Main {
 		    	System.out.println("Le niveau n'est pas valide.");
 		    }
 		});*/
-		Node start = new Node(lvl1.findRobot(), lvl1.findRobot(), lvl1.getLvl()[11][55], null);
-		Nodelist openList = new Nodelist(start);
-		System.out.println(openList);
-		
-		
+		System.out.println("Robot : {" + lvl1.findRobot().getPosX() + ";" + lvl1.findRobot().getPosY() + "}");
+		Node start = new Node(lvl1.findRobot(), lvl1.findRobot(), lvl1.getLvl()[2][5], null);
+		Node end = new Node(lvl1.getLvl()[11][55], lvl1.findRobot(), lvl1.getLvl()[2][5], null);
+		List<Node> path = findPath(start, end, lvl1);
+//		System.out.println(path);
 	}
 }
--- src/pathfinding/Node.java
+++ src/pathfinding/Node.java
@@ -17,8 +17,48 @@ public class Node {
 		this.parent = parent;
 	}
 	
+	public float getCostStart() {
+		return this.costStart;
+	}
+	
+	public Element getCurrent() {
+		return this.current;
+	}
+	
+	public float getCostDest() {
+		return this.costDest;
+	}
+	
+	public float getQuality() {
+		return this.quality;
+	}
+	
+	public Node getParent() {
+		return this.parent;
+	}
+	
+	public void setParent(Node parent) {
+		this.parent = parent;
+	}
+	
 	@Override
 	public String toString() {
 		return "Parent : " + this.parent + "; Current : " + this.current + "; Cost to Start : " + this.costStart + "; Cost to dest : " + this.costDest + "; Quality : " + this.quality;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if(o.getClass() == this.getClass()) {
+			Node n = (Node)o;
+			if(n.current.getPosX() == this.current.getPosX() && n.current.getPosY() == this.current.getPosY()) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	@Override
+	public int hashCode() {
+		return this.hashCode() ^ this.current.hashCode();
 	}
 }
